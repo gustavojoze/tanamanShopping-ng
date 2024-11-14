6b7b56ff25bc780aca41340d35a883dcbde83a05
gustavojoze <gustavojosesoaressantana@gmail.com>
2024-11-14 17:41:08 -0300

    Criando produto service

--- src/app/model/produto.ts
+++ src/app/model/produto.ts
@@ -1,8 +1,9 @@
 export class Produto {
-    public codigo: number = 0
+    public codigo: number = 0;
     public nome: string = "";
     public descricao: string = "";
     public valor: number = 0;
     public quantidade: number = 0;
     public keywords: string = "";
+    public destaque: number = 0;
 }
