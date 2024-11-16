f87a909fe32ef73e5cdf99b1c713e3edf6b8d34a
gustavojoze <gustavojosesoaressantana@gmail.com>
2024-11-16 15:40:26 -0300

    Deixando a classe produto sem gerar codigo automatico.

--- backend/loja/src/main/java/com/fatec/loja/Produto.java
+++ backend/loja/src/main/java/com/fatec/loja/Produto.java
@@ -1,14 +1,11 @@
 package com.fatec.loja;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Produto {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO) 
     private int codigo;
     private String nome;
     private String descricao;
